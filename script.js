4f3905e14ba81c50582d06608128f1cc4334b01b
Suman Kisku <sumankisku1@gmail.com>
2023-02-11 20:30:02 +0530

    fixed little bit of ui

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function renderProducts() {
                 <div class="title"><b>Title:</b><span>${item.title}</span></div>
                 <div class="brand"><b>Brand:</b><span>${item.brand}</span></div>
             </div>
-            <div class="price">${item.price}</div>
-            <div class="rating">${item.rating}</div>
-            <div class="stock">${item.stock}</div>
+            <div class="price">$${item.price}</div>
+            <div class="rating">Rating: ${item.rating}</div>
+            <div class="stock">Stock: ${item.stock}</div>
         </div>
         
     </div>
